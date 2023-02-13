f9692b133dcf8ef87ee8a15964bd4a0445e147c8
Elizabeth <qazwsx050816@gmail.com>
2023-02-13 18:35:39 +0300

    add 4 homework

---
+++ Zadanie25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Exponentiation(int A, int B)
+{
+  int result = 1;
+
+  for(int count=1; count <= B; count++)
+  {
+    result = result * A;
+  }
+    return result;
+}
+
+  Console.WriteLine("Введите число A ");
+  int.TryParse(Console.ReadLine(), out int A);
+  Console.WriteLine("Введите число B ");
+  int.TryParse(Console.ReadLine(), out int B);
+
+  int newExponentiation = Exponentiation(A, B);
+  Console.WriteLine($"{A} в степени {B} равно {newExponentiation}");
---
+++ Zadanie27!/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+int calculateSum(int number)
+{
+int sum = 0;
+string s = number.ToString();
+
+for (int i = 0; i < s.Length; i++)
+{
+    sum = sum + int.Parse(s[i].ToString());
+}
+
+return sum;
+}
+
+int newSum = calculateSum(number);
+Console.WriteLine($"Сумма цифр в числе {number} равна {newSum}");
